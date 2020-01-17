d79a772d5b437ab06f257b6fb8b60e531f7c39eb
聂苗 <niemiao@intra.nsfocus.com>
2020-01-17 18:06:29 +0800

    add

---
+++ case/baidu.test.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer');
+
+test('baidu title is correct', async () => {
+  const browser = await puppeteer.launch(
+  {args:['--no-sandbox']}
+ );
+  const page = await browser.newPage();
+  await page.goto('https://baidu.com');
+  const title = await page.title();
+  expect(title).toBe('百度一下，你就知道');
+  await browser.close();
+});
